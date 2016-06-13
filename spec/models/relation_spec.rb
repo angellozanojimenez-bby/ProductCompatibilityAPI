d5758e59dacd5121b43a022276f8cb6d8f283ec8
Angel Lozano Jimenez <angellozanojimenez.bestbuy@gmail.com>
2016-06-13 16:42:45 -0500

    Forgot Shoulda Matchers for the Relation spec.

--- spec/models/relation_spec.rb
+++ spec/models/relation_spec.rb
@@ -9,4 +9,8 @@ describe Relation do
   it { should respond_to(:sku_two) }
   it { should respond_to(:relation_type) }
 
+  it { should validate_presence_of(:sku_one) }
+  it { should validate_presence_of(:sku_two) }
+  it { should validate_presence_of(:relation_type) }
+
 end
