8d128cc5a1cace3548e9290173710998047b8091
Angel Lozano Jimenez <angellozanojimenez.bestbuy@gmail.com>
2016-06-14 08:54:39 -0500

    Updated application controller to prevent CSRF exception from being raised.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::UsersController < ApplicationController
   respond_to :json
 
+  # Find user by the parameters into the function.
   def show
     respond_with User.find(params[:id])
   end
+  
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,20 +1,25 @@
 require 'spec_helper'
 
 describe Api::V1::UsersController do
+  # Within the UsersController's spec, before each test/describe, request the
+  # following headers into our HTTP request.
   before(:each) { request.headers['Accept']  = "application/vnd.productcompatibility.v1" }
 
   describe "GET #show" do
+    # We create a fake user using FactoryGirl and we GET/#show the user
+    # by their user id.
     before(:each) do
       @user = FactoryGirl.create :user
       get :show, id: @user.id, format: :json
     end
-
+    # We expect the response within the JSON body to contain the email
+    # which is associated with the user.
     it "returns the information about a reporter on a hash" do
       user_response = JSON.parse(response.body, symbolize_names: true)
       expect(user_response[:email]).to eql @user.email
     end
-
+    # We expect a 200 code response.
     it { should respond_with 200 }
   end
-  
+
 end
