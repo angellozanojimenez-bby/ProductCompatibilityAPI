9a35d6fab6616d39430345cd596d2eb57eb20fa2
Angel Lozano Jimenez <angellozanojimenez.bestbuy@gmail.com>
2016-06-22 12:58:57 -0500

    JSON Relationship object is now being returned correctly.

--- app/controllers/api/v1/relationships_controller.rb
+++ app/controllers/api/v1/relationships_controller.rb
@@ -10,14 +10,6 @@ class Api::V1::RelationshipsController < ApplicationController
     respond_with Relationships.find(params[:id])
   end
 
-  # elsewhere
-  # rel = ManagedRel.new(from_node: user, to_node: any_node)
-  #  if rel.save
-  # validation passed, to_node is a manageable object
-  # else
-  # something is wrong
-  # end
-
   def create
     relationship = Relationships.new(relationship_params)
 
@@ -31,11 +23,12 @@ class Api::V1::RelationshipsController < ApplicationController
     else
       @product_two = FactoryGirl.create :product_nodes
     end
-
-    relationship_graph = Relationships.create(from_node: @product_one, to_node: @product_two,
+    # Create relationship in database.
+    relationship = Relationships.create(from_node: @product_one, to_node: @product_two,
     employee_number: relationship.employee_number, notes: relationship.notes,
     primary_node_sku: @product_one.sku, secondary_node_sku: @product_two.sku)
-    if relationship.save && relationship_graph.save
+
+    if relationship.save
       render json: relationship, status: 201, location: [:api, relationship]
     else
       render json: { errors: relationship.errors }, status: 422
