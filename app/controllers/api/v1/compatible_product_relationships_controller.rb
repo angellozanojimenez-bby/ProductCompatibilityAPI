e99da95e7df314bd4f5478a6be40da3d472e9737
Angel Lozano Jimenez <angellozanojimenez.bestbuy@gmail.com>
2016-07-21 13:21:47 -0500

    Added an extra endpoint to retrieve the incompatible relationships of a given product by the sku number.

--- app/controllers/api/v1/compatible_product_relationships_controller.rb
+++ app/controllers/api/v1/compatible_product_relationships_controller.rb
@@ -1,6 +1,6 @@
 require 'httparty'
 
-class Api::V1::ProductRelationshipsController < ApplicationController
+class Api::V1::CompatibleProductRelationshipsController < ApplicationController
   respond_to :json
   # These methods take care of handling the creation, showing, updating and deletion
   # of the relationships in Neo4j.
@@ -8,7 +8,7 @@ class Api::V1::ProductRelationshipsController < ApplicationController
   def index
     respond_with Relationships.all
   end
-  
+
   def show
     respond_with Relationships.where(primary_node_sku_or_upc: params[:primary_node_sku_or_upc].to_i)
   end
